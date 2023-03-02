548357254f58958724d358770eb46f955115eea8
caoxiaoyue <caoxiaoyue@ones.ai>
2023-03-02 15:35:58 +0800

    feat: 去除冗余代码&&新增部分注释

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,30 +2,27 @@ import { useRef} from 'react'
 import html2canvas from 'html2canvas'
 import canvas2image from "canvas2image-2";
 import './index.css';
-import { Button } from 'antd';
-import 'antd/dist/reset.css';
 export const App = () => {
+    // 获取模版dom
     const divf = useRef()
+    // 获取bmp图片函数
     const getImg = async () => {
         // dom转成canvas
-        html2canvas(divf.current,).then((canvas) => {
-            const newCanvas = document.body.appendChild(canvas);
-            // canvas 转成bmp
-            const img =  canvas2image.convertToBMP(canvas)
-            // 下载bmp图片
-            img.onload = () => {
-                const a = document.createElement('a');
-                a.download = 'test';
-                a.href = img.src;
-                a.click();
-                // 删除canvas
-                newCanvas.remove();
-
-            }
-        })
+        const canvas = await html2canvas(divf.current)
+        // canvas转bmp
+        const img =  canvas2image.convertToBMP(canvas)
+        // 下载bmp图片
+        img.onload = () => {
+            const a = document.createElement('a');
+            // 设置下载文件名
+            a.download = 'test';
+            a.href = img.src;
+            a.click();
+        }
     }
     return (
         <div className="App">
+           {/*模版dom*/}
             <div ref={divf} class="divf">
                 <div class="divf2">
                     <div class="divline">
@@ -45,9 +42,9 @@ export const App = () => {
                     </div>
                 </div>
             </div>
-            <Button className={'btn'} onClick={async ()=>{await getImg()}}>
+            <button className={'btn'} onClick={async ()=>{await getImg()}}>
                 下载bmp图片
-            </Button>
+            </button>
         </div>
 
     );
